91bd362f36795e6e208a1982c981b7287d65a00e
Gunnar Torfi <gunnartorfis@gmail.com>
2024-09-05 16:07:39 +0000

    chore: match sonner's api for updating toasts

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-export { Toast } from './toast';
 export { Toaster } from './toaster';
 export { toast } from './toast-fns';
 export { ToastVariant, ToastPosition, ToastSwipeDirection } from './types';
