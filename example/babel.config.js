b0f19fa720633bc20b3b2d7bc31eb1634892f7f0
Gunnar Torfi <gunnartorfis@gmail.com>
2025-07-05 09:56:19 +0000

    chore: prettier

--- example/babel.config.js
+++ example/babel.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const { getConfig } = require('react-native-builder-bob/babel-config');
 const pkg = require('../package.json');
 
-// eslint-disable-next-line no-undef
 const root = path.resolve(__dirname, '..');
 
 module.exports = function (api) {
